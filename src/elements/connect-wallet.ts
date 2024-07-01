c9c8a4b7735a08cc0d89192a3ace5c159a23e85b
VandeurenGlenn <8685280+VandeurenGlenn@users.noreply.github.com>
2024-07-01 12:58:26 +0200

    Create connect-wallet.ts

---
+++ src/elements/connect-wallet.ts
@@ -0,0 +1,33 @@
+import { LitElement, html, css } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+import '@material/web/button/filled-button.js'
+@customElement('connect-wallet')
+export class ConnectWallet extends LitElement {
+  static styles = [
+    css`
+      :host {
+        margin-top: 12px;
+        display: block;
+        width: 100%;
+      }
+
+      md-filled-button {
+        width: 100%;
+        height: 56px;
+        font-size: 20px;
+        font-weight: 600;
+        border-radius: var(--border-radius);
+        --md-filled-button-container-color: var(--special-accent);
+        --md-filled-button-label-text-color: var(--on-special-accent);
+        --md-filled-button-hover-label-text-color: var(--on-special-accent);
+        --md-filled-button-pressed-label-text-color: var(--on-special-accent);
+        --md-filled-button-active-label-text-color: var(--on-special-accent);
+        --md-filled-button-focus-label-text-color: var(--on-special-accent);
+      }
+    `
+  ]
+
+  render() {
+    return html` <md-filled-button>connect wallet</md-filled-button>`
+  }
+}
