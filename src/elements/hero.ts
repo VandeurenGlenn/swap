50af4371825b76abe4c4576e5be1bdc40fc53e2d
VandeurenGlenn <8685280+VandeurenGlenn@users.noreply.github.com>
2024-07-01 12:58:29 +0200

    Create hero.ts

---
+++ src/elements/hero.ts
@@ -0,0 +1,35 @@
+import { LitElement, html, css } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+import '@material/web/elevation/elevation.js'
+
+@customElement('hero-element')
+export class HeroElement extends LitElement {
+  static styles = [
+    css`
+      :host {
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        max-width: 480px;
+        width: 100%;
+        height: 100%;
+        max-height: 320px;
+        border-radius: var(--border-radius);
+        background-color: var(--surface-1);
+        color: var(--on-surface-1);
+        padding: 6px;
+      }
+
+      md-elevation {
+        --md-elevation-level: 1;
+      }
+    `
+  ]
+
+  render() {
+    return html`
+      <md-elevation level="1"></md-elevation>
+      <slot></slot>
+    `
+  }
+}
